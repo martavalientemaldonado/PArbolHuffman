efba20d59cba7ac11731afd6d5e7df89f0dbf6d6
candelasanzm <sanzmartincandela@gmail.com>
2024-10-25 10:54:16 +0200

    Clases

--- src/main/scala/arbolHuffman.scala
+++ src/main/scala/arbolHuffman.scala
@@ -10,3 +10,14 @@ case class RamaHuff(nodoIzq : HojaHuff, nodoDch : HojaHuff, chars : List[Char], 
   def caracteres() : List[Char] = this match
     case RamaHuff(nodoIzq, nodoDch, chars, peso) => chars
     case HojaHuff(char, peso) => List(char)
+
+  def decodificar(bits : List[Bit]) : String =
+    def decodificarAux(bits : List[Bit], acc : String) : String = this match
+      case HojaHuff(char, peso) if bits.isEmpty => acc
+      case HojaHuff(char, peso) => decodificarAux(bits, acc)
+
+def codificar(cadena : String) : List[Bit]
+
+
+object miPrograma extends App
+  val miArbol = RamaHuff(RamaHuff(HojaHuff, HojaHuff, List("E", " "), peso = 4), RamaHuff(HojaHuff, HojaHuff, List("S", "O"), peso = 7), )
---
+++ src/main/scala/auxiliar.scala
@@ -0,0 +1,13 @@
+class codificarYDecodificar
+  type Bit = 0 | 1
+  
+  def cadenaAListaChars(cadena : String) : List[Char] = cadena.toList
+
+  def listaCharsACadena(listaCar : List[Char]) : String =
+    def listaCharsACadenaAux(listaCar : List[Char], acc : String) : String = listaCar match
+      case Nil => acc
+      case head :: tail => listaCharsACadenaAux(listaCar, acc + head)
+      
+    listaCharsACadenaAux(listaCar, " ")
+
+  

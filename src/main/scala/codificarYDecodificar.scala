714f978442e8fa312a131521377ee1a58e55bbf0
candelasanzm <sanzmartincandela@gmail.com>
2024-10-25 10:33:33 +0200

    Clases

--- src/main/scala/codificarYDecodificar.scala
+++ src/main/scala/codificarYDecodificar.scala
@@ -1,17 +1,11 @@
 class codificarYDecodificar
   type Bit = 0 | 1
   
-  def cadenaAListaChars(cadena : String) : List[Char] =
-    def cadenaAListaCharsAux(cadena : String, temp : Int) : List[Char] = 
-
-    cadenaAListaCharsAux()
+  def cadenaAListaChars(cadena : String) : List[Char] = cadena.toList
 
   def listaCharsACadena(listaCar : List[Char]) : String =
     def listaCharsACadenaAux(listaCar : List[Char], temp : Int) : String = listaCar match
-      case Nil => 
-      case _ => listaCharsACadenaAux(listaCar, temp + 1)
-      
-    listaCharsACadenaAux(listaCar, 0)
+      case
 
   def decodificar(bits : List[Bit]) : String
 
